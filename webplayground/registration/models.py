ba8471d40fc8af47e4e06edd4309bf336169b3a6
RodGar <rodrigogarciaavila26@gmail.com>
2019-07-23 01:16:31 -0500

    Pruebas unitarias y signals
    Una signal es un disparador que se puede ejecutar después, antes o al ser eliminada una instancia.
    
    Una prueba unitaria se realiza en el fichero test,py del modelo al cual se desee realizar una prueba unitaria

--- webplayground/registration/models.py
+++ webplayground/registration/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.dispatch import receiver # Decorator
+from django.db.models.signals import post_save
 # Create your models here.
 class Profile(models.Model):
     #Let's create a relation with a user model
@@ -7,3 +9,11 @@ class Profile(models.Model):
     avatar = models.ImageField(upload_to='profiles',null=True,blank=True)
     bio = models.TextField(null=True,blank=True)
     link = models.URLField(max_length=200, null=True, blank=True)
+
+#Signal
+@receiver(post_save,sender=User) #User is a model which sends the signal o execute the trigger
+def ensure_profile_exists(sender,instance,**kwargs):
+    if kwargs.get('created',False ):
+        #If exisits 'created' it's the first time that the instance has been created
+        Profile.objects.get_or_create(user=instance)
+        print("-----Profile created-----")
--- webplayground/registration/tests.py
+++ webplayground/registration/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
-
+from .models import Profile
+from django.contrib.auth.models import User
 # Create your tests here.
+#Prueba unitaria
+class ProfileTestCase(TestCase):
+    def setUp(self):
+        #Let's create an unser of testing
+        User.objects.create_user('test','test@test.com','testqwerty12')
+    #Allways the testing method has to strat with 'test_'
+    def test_profile_exists(self):
+        exixts = Profile.objects.filter(user__username='test').exists()
+        #Execute the test case the variale exists has to be TRUE
+        self.assertEqual(exixts,True)
+
+        #To execute the test open the terminal and run:
+        #python manage,py test registration
+
