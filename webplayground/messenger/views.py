654ebbe707b1ab894d88fbc9e4eef332c69a5ee7
RodGar <rodrigogarciaavila26@gmail.com>
2019-07-30 14:56:38 -0500

    Mensaje asícrono y eventos de botón

--- webplayground/messenger/views.py
+++ webplayground/messenger/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from .models import Thread
+from .models import Thread, Message
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 from django.views.generic import TemplateView
 from django.http import Http404, JsonResponse
+from django.shortcuts import  get_object_or_404
 
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
@@ -33,4 +34,16 @@ def add_message(request,pk):
     # Cada vez que se envie un mensaje se devolverá un arespuesta
     # Si el msg se envia conrrectamente se cambiara a True
     jason_response = {'created':False}
+    # Comproabamos si el usuario esta idenificado
+    if request.user.is_authenticated:
+        # Recuperamos el contenido del text area cuyo id es 'content'
+        content = request.GET.get('content',None)
+        if content:
+            thread = get_object_or_404(Thread,pk=pk)
+            message = Message.objects.create(user=request.user,conent=content)
+            # añadimos el mensaje al hilo
+            thread.messages.add(message)
+            jason_response['created']=True
+    else:
+        raise Http404("User is not authenticated")
     return JsonResponse(jason_response) #Retorna un obj Json
